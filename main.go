f4e5522b1338719845904078064a85878a10dc2c
Edwin Garcia <edwinprogramando@gmail.com>
2023-10-11 11:09:53 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"log"
+	"sync"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+var websocketUpgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+type ClientList map[*Client]bool
+
+type Client struct {
+	connection *websocket.Conn
+	manager    *Manager
+	egress     chan []byte
+}
+
+type Manager struct {
+	clients ClientList
+	sync.RWMutex
+}
+
+func NewManager() *Manager {
+	return &Manager{
+		clients: make(ClientList),
+	}
+}
+
+func NewClient(conn *websocket.Conn, manager *Manager, c chan []byte) *Client {
+	return &Client{
+		connection: conn,
+		manager:    manager,
+		egress:     c,
+	}
+}
+
+func main() {
+	r := gin.Default()
+
+	manager := NewManager()
+
+	r.GET("/ws", manager.serveWS)
+
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func (m *Manager) serveWS(c *gin.Context) {
+	conn, err := websocketUpgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	ch := make(chan []byte)
+
+	client := NewClient(conn, m, ch)
+
+	m.addClient(client)
+
+	go client.writeMessages()
+
+	go messagesGenerator(ch)
+}
+
+func messagesGenerator(ch chan []byte) {
+	for {
+		time.Sleep(time.Second * 5)
+
+		ch <- []byte("hello from ws!")
+	}
+}
+
+func (m *Manager) addClient(client *Client) {
+	m.Lock()
+	defer m.Unlock()
+
+	m.clients[client] = true
+}
+
+func (m *Manager) removeClient(client *Client) {
+	m.Lock()
+	defer m.Unlock()
+
+	if _, ok := m.clients[client]; ok {
+		client.connection.Close()
+		delete(m.clients, client)
+	}
+}
+
+func (c *Client) writeMessages() {
+	defer func() {
+		// Graceful close if this triggers a closing
+		c.manager.removeClient(c)
+	}()
+
+	for {
+		select {
+		case message, ok := <-c.egress:
+			// Ok will be false Incase the egress channel is closed
+			if !ok {
+				// Manager has closed this connection channel, so communicate that to frontend
+				if err := c.connection.WriteMessage(websocket.CloseMessage, nil); err != nil {
+					// Log that the connection is closed and the reason
+					log.Println("connection closed: ", err)
+				}
+				// Return to close the goroutine
+				return
+			}
+			// Write a Regular text message to the connection
+			if err := c.connection.WriteMessage(websocket.TextMessage, message); err != nil {
+				log.Println(err)
+			}
+			log.Println("sent message")
+		}
+
+	}
+}
